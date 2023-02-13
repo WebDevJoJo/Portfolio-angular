51d9cb3144c11a30ed59601591269602016f80d9
Mattia280397 <121868385+Mattia280397@users.noreply.github.com>
2023-02-13 17:34:36 +0100

    Primo Commit
    Fatto About Us e Portfolio slideshow manca solo da agganciare il js

--- JoeleMattia-portfolio/src/app/app-routing.module.ts
+++ JoeleMattia-portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'aboutus', component: AboutusComponent},
+ { path: 'portfolio', component: PortfolioComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
