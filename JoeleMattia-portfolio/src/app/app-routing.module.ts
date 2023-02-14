c594b5a9ceb9aa0733e5b537b92379a84cfc1550
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-14 14:15:39 +0100

    routes adjusted

--- JoeleMattia-portfolio/src/app/app-routing.module.ts
+++ JoeleMattia-portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
