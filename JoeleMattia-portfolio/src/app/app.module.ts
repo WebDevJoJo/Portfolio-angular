8998d93f42c5f9a1a9b9619e8628c6ad8c562b0d
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-14 15:39:02 +0100

    merge defined

--- JoeleMattia-portfolio/src/app/app.module.ts
+++ JoeleMattia-portfolio/src/app/app.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-
-@NgModule({
-  declarations: [],
-  imports: [BrowserModule, AppRoutingModule],
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +17,11 @@ import { CarouselComponent } from './carousel/carousel.component';
     AboutusComponent,
     PortfolioComponent,
     CarouselComponent,
-    FooterComponent, HomeComponent, ContactUsComponent
+    FooterComponent,
+    HomeComponent,
+    ContactUsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
