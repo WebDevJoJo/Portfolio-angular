10f16b3dfcba243b1ff34b463948bdb34c805541
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-15 15:52:13 +0100

    J merged

--- JoeleMattia-portfolio/src/assets/Carousel.js
+++ JoeleMattia-portfolio/src/assets/Carousel.js
@@ -1,15 +1,15 @@
 function priceCalculate() {
   const inputs = document.getElementsByClassName("form-check-input");
   const arr = []; // Array to store the checked values
-  const mytext=document.getElementById('TotText');
+  const mytext = document.getElementById("TotText");
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].checked) {
       arr.push(parseInt(inputs[i].value));
-      console.log(inputs[i].value);
     }
   }
-  mytext.className="visible"; 
-  let tot=parseInt(arr.reduce((accumulator,currentValue)=>accumulator+currentValue,0))
- mytext.value = tot;
+  mytext.className = "visible";
+  let tot = parseInt(
+    arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  );
+  mytext.value = tot;
 }
-
