1c3c87beaa704f94c3cfacba082e4d0f482d5ea3
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-16 12:29:32 +0100

    script file added

---
+++ JoeleMattia-portfolio/src/assets/script/script.js
@@ -0,0 +1,14 @@
+function priceCalculate() {
+  const inputs = document.getElementsByClassName('form-check-input');
+  const arr = []; // Array to store the checked values
+  const mytext = document.getElementById('TotText');
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      arr.push(parseInt(inputs[i].value));
+    }
+  }
+  mytext.className = 'visible';
+  mytext.value =
+    'Everything will cost ' +
+    arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
