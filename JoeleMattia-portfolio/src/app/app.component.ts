3a34578cc730953a4d8e71ffc971814408ad23ef
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-16 12:54:23 +0100

    bug fixed

--- JoeleMattia-portfolio/src/app/app.component.ts
+++ JoeleMattia-portfolio/src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-declare var priceCalculate: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    new priceCalculate();
-  }
-  title = 'Joele Mattia portfolio';
-}
+export class AppComponent {}
