08689e29aef13a724dd9be89f5d84072602c4c90
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-16 11:48:32 +0100

    app routing module ts written

--- JoeleMattia-portfolio/src/app/app-routing.module.ts
+++ JoeleMattia-portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PreventiveComponent } from './preventive/preventive.component';
+const routes: Routes = [
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'preventive', component: PreventiveComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
