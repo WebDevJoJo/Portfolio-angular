f390da3a80d47dba380b7464fdc5f4f7ed0cc020
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-16 11:52:02 +0100

    preventive component ts written

--- JoeleMattia-portfolio/src/app/preventive/preventive.component.ts
+++ JoeleMattia-portfolio/src/app/preventive/preventive.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-preventive',
   templateUrl: './preventive.component.html',
-  styleUrls: ['./preventive.component.scss']
+  styleUrls: ['./preventive.component.scss'],
+  template: `<button
+    type="button"
+    (click)="priceCalculate()"
+    class="btn btn-outline-light"
+  >
+    Calculate
+  </button> `,
 })
 export class PreventiveComponent {
-
+  priceCalculate() {
+    new priceCalculate();
+  }
 }
