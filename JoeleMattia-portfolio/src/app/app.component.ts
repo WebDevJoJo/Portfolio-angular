8c1cc0981fccd9cf6512b63c61f0e61842c055b8
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-16 11:35:57 +0100

    app component ts written

--- JoeleMattia-portfolio/src/app/app.component.ts
+++ JoeleMattia-portfolio/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var priceCalculate: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'JoeleMattia-portfolio';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    new priceCalculate();
+  }
+  title = 'Joele Mattia portfolio';
 }
