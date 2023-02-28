026b1ed0fd85155b56c3cbe7e622b7824840431a
webdevjojo <joele.melchiorre@edu.itspiemonte.it>
2023-02-28 17:31:50 +0100

    preventive ts file formatted with prettier

--- JoeleMattia-portfolio/src/app/preventive/preventive.component.ts
+++ JoeleMattia-portfolio/src/app/preventive/preventive.component.ts
@@ -5,42 +5,46 @@ import { Component, ElementRef, Input } from '@angular/core';
   styleUrls: ['./preventive.component.scss'],
 })
 export class PreventiveComponent {
-clicked:boolean=false;
-tmparray:number[]=[];
-urlmodel:boolean=true;
-fullname: any;
-email:any;
-type:any="50";
-Manteinance:any="0";
-showthis:boolean=false;
-date:any;
-urltocalc:any=50;
-result:number=0;
-urlgroup:string[]= ['www.pippo.it', 'wwww.lol.it', 'www.xd.com'];
-checkthis:string="";
-priceCalculate(){
-  if(this.email && this.fullname && this.date)
-  {
-    this.clicked=true;
-    if(this.urltocalc!=50){
-      this.urltocalc=0;
+  clicked: boolean = false;
+  tmparray: number[] = [];
+  urlmodel: boolean = true;
+  fullname: any;
+  email: any;
+  type: any = '50';
+  Manteinance: any = '0';
+  showthis: boolean = false;
+  date: any;
+  urltocalc: any = 50;
+  result: number = 0;
+  urlgroup: string[] = ['www.pippo.it', 'wwww.lol.it', 'www.xd.com'];
+  checkthis: string = '';
+  priceCalculate() {
+    if (this.email && this.fullname && this.date) {
+      this.clicked = true;
+      if (this.urltocalc != 50) {
+        this.urltocalc = 0;
+      }
+      this.tmparray.push(
+        parseInt(this.type),
+        parseInt(this.urltocalc),
+        parseInt(this.Manteinance)
+      );
+      this.result = this.tmparray.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      );
+      this.tmparray = [];
+    } else {
+      alert('nooooh');
     }
-      this.tmparray.push(parseInt(this.type),parseInt(this.urltocalc), parseInt(this.Manteinance));
-      this.result=this.tmparray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-      this.tmparray=[];
   }
-  else{
-    alert("nooooh");
-  }
-}
   eshopMethods() {
-    this.showthis=true;
+    this.showthis = true;
   }
-  hideEshop(){
-    this.showthis=false;
+  hideEshop() {
+    this.showthis = false;
   }
   CheckUrl() {
-    
     if (this.urlgroup.includes(this.checkthis)) {
       alert('Il link non è disponibile');
     } else if (this.checkthis == '') {
